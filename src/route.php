34b425fc7743fb9a0aa72a5a4071ea639c17124b
ado-work <114458573@qq.com>
2018-03-16 12:40:17 +0800

    add readme

--- src/route.php
+++ src/route.php
@@ -14,36 +14,29 @@ use Ado\Shopify\Http\Middleware\HasValidHostname;
 use Ado\Shopify\Http\Middleware\HasValidNonce;
 use Ado\Shopify\Http\Middleware\RedirectIfAuthenticated;
 
-Route::group(['namespace' => 'NickyWoolf\\Carter\\Http\\Controllers'], function (Router $router) {
-
+Route::group(['namespace' => 'Ado\\Shopify\\Http\\Controllers'], function (Router $router) {
     /* Guest Routes */
     $router->group(['middleware' => [RedirectIfAuthenticated::class]], function (Router $router) {
-
-        $router->get('shopify/signup', 'InstalledAppsController@create')->name('carter.signup');
-
+        $router->get('shopify/signup', 'InstalledAppsController@create')->name('shopify.signup');
         $router->match(['get', 'post'], 'shopify/install', 'InstalledAppsController@store')->middleware([
             HasShopDomain::class,
-        ])->name('carter.install');
+        ])->name('shopify.install');
 
         $router->get('shopify/register', 'RegisteredShopsController@store')->middleware([
             HasValidNonce::class,
             HasValidHmac::class,
             HasValidHostname::class,
-        ])->name('carter.register');
+        ])->name('shopify.register');
 
         $router->get('shopify/login', 'AuthenticatedShopsController@store')->middleware([
             HasShopDomain::class,
-        ])->name('carter.login');
-
-        $router->get('/shopify/expired_session', 'ExpiredSessionsController@index')->name('carter.expired-session');
-
+        ])->name('shopify.login');
+        $router->get('/shopify/expired_session', 'ExpiredSessionsController@index')->name('shopify.expired-session');
     });
 
     /* Auth Routes */
     $router->group(['middleware' => [Authenticate::class]], function (Router $router) {
-
-        $router->get('shopify/dashboard', 'DashboardController@index')->name('carter.dashboard');
-
+        $router->get('shopify/dashboard', 'DashboardController@index')->name('shopify.dashboard');
     });
 
 });
